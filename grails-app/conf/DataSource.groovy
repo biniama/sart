62bef7c157dfc44f1e9f08be72acf33af5a3833d
Biniam Asnake Kefale <biniamasnake@gmail.com>
2017-09-13 12:04:32 +0200

    test DS changed 2

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,28 +1,15 @@
-/*
- To use MyISAM, your configuration will look like this:
- dataSource {
- pooled = true
- driverClassName = "com.mysql.jdbc.Driver"
- dialect = 'org.hibernate.dialect.MySQL5Dialect'
- }
- To use InnoDB, your configuration should look like:
- dataSource {
- pooled = true
- driverClassName = "com.mysql.jdbc.Driver"
- dialect = 'org.hibernate.dialect.MySQL5InnoDBDialect'
- }
- */
-
 dataSource {
-	pooled = true
-	driverClassName = "com.mysql.jdbc.Driver"
-	dialect = "org.hibernate.dialect.MySQL5InnoDBDialect"
-	url = "jdbc:mysql://localhost:3306/sart?useUnicode=yes&characterEncoding=UTF-8"
+    pooled = true
+    driverClassName = "org.postgresql.Driver"
+    dialect = "org.hibernate.dialect.PostgreSQLDialect"
 }
 hibernate {
-	cache.use_second_level_cache = true
-	cache.use_query_cache = false
-	cache.region.factory_class = 'net.sf.ehcache.hibernate.EhCacheRegionFactory'
+    cache.use_second_level_cache = true
+    cache.use_query_cache = false
+//    cache.region.factory_class = 'net.sf.ehcache.hibernate.EhCacheRegionFactory' // Hibernate 3
+    cache.region.factory_class = 'org.hibernate.cache.ehcache.EhCacheRegionFactory' // Hibernate 4
+    singleSession = true // configure OSIV singleSession mode
+    flush.mode = 'manual' // OSIV session flush mode outside of transactional context
 }
 
 // environment specific settings
